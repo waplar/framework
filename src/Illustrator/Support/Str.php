4b018a8384e323c605a17776c0c00b0d4334eb59
KanekiYuto <kaneki.yuto.404@gmail.com>
2025-05-21 20:51:42 +0800

    feat(Support): 添加字符串工具类 Str
    - 新增 Str 类，提供字符串处理相关的方法
    - 实现了生成列别名 SQL 和 ORM with 关联加载的功能

---
+++ src/Illustrator/Support/Str.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Illustrator\Support;
+
+class Str
+{
+
+    /**
+     * 生成列别名 SQL
+     * Generate column aliases SQL
+     *
+     * @param  string  $source
+     * @param  string  $target
+     *
+     * @return string
+     */
+    public static function columnAlias(string $source, string $target): string
+    {
+        return implode(' as ', [$source, $target]);
+    }
+
+    /**
+     * 生成多个 ORM with 关联加载
+     * Generate multiple ORMs with associated loading
+     *
+     * @param  array  $relations
+     *
+     * @return array
+     */
+    public function ormWithMany(array $relations): array
+    {
+        return collect($relations)->map(function ($columns, $name) {
+            return $name . (!empty($columns) ? ':' . implode(',', $columns) : '');
+        })->values()->toArray();
+    }
+
+
+}
